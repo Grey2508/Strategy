0efc156ebb8274ab6643ba8aa6e1e50fa5dda516
Grey2508 <grey-_@mail.ru>
2022-11-04 05:39:47 +0500

    Update StartGame.cs
    Change Call method in Invoke

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -10,7 +10,7 @@ public class StartGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("ActiveEnemyCreators", TimeToActiveEnemyCreators);
+        Invoke(nameof(ActiveEnemyCreators), TimeToActiveEnemyCreators);
     }
 
     void ActiveEnemyCreators()
