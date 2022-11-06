8875a8ef1af40775884efb326ac850c86b99a48c
Grey2508 <grey-_@mail.ru>
2022-11-06 05:25:24 +0500

    Move scripts Enemy.cs, Knight.cs, Miner.cs and Unit.cs to Unit folder

---
+++ Assets/Scripts/Units/Enemy.cs
@@ -0,0 +1,280 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum EnemyState
+{
+    Idle,
+    WalkToBuilding,
+    WalkToUnit,
+    AttackUnit,
+    AttackBuilding
+}
+
+public class Enemy : MonoBehaviour
+{
+    public EnemyState CurrentEnemyState;
+
+    public int Health = 5;
+    public GameObject HealthBarPrefab;
+
+    public Building TargetBuilding;
+    public Unit TargetUnit;
+
+    public float AttackRange = 1;
+    public float FollowRange = 7;
+
+    public NavMeshAgent NavMeshAgent;
+
+    public float AttackPeriod = 1;
+    public int Damage = 1;
+
+    private float _timer;
+    private int _maxHealth;
+    private HealthBar _healthBar;
+
+    private void Start()
+    {
+        Management.AddEnemy(this);
+
+        GameObject healthBarPrefab = Instantiate(HealthBarPrefab);
+        _healthBar = healthBarPrefab.GetComponent<HealthBar>();
+        _healthBar.Setup(transform);
+
+        _maxHealth = Health;
+
+        SetState(EnemyState.WalkToBuilding);
+    }
+    void Update()
+    {
+        switch (CurrentEnemyState)
+        {
+            case EnemyState.Idle:
+                EnemyIdle();
+                break;
+            case EnemyState.WalkToBuilding:
+                EnemyWalkToBuilding();
+                break;
+            case EnemyState.AttackBuilding:
+                EnemyAttackBuilding();
+                break;
+            case EnemyState.WalkToUnit:
+                EnemyWalkToUnit();
+                break;
+            case EnemyState.AttackUnit:
+                EnemyAttackUnit();
+                break;
+        }
+    }
+
+    private void EnemyIdle()
+    {
+        FindClosestBuilding();
+
+        if (TargetBuilding)
+        {
+            SetState(EnemyState.WalkToBuilding);
+            return;
+        }
+
+        FindClosestUnit();
+    }
+
+    private void EnemyWalkToBuilding()
+    {
+        FindClosestUnit();
+
+        if (!TargetBuilding)
+        {
+            SetState(EnemyState.Idle);
+            return;
+        }
+
+        float distance = Vector3.Distance(transform.position, TargetBuilding.transform.position);
+        if (distance < AttackRange)
+            SetState(EnemyState.AttackBuilding);
+    }
+
+    private void EnemyAttackBuilding()
+    {
+        if (!TargetBuilding)
+        {
+            SetState(EnemyState.WalkToBuilding);
+            return;
+        }
+
+        float distance = Vector3.Distance(transform.position, TargetBuilding.transform.position);
+
+        if (distance > AttackRange)
+        {
+            SetState(EnemyState.WalkToBuilding);
+        }
+
+        _timer += Time.deltaTime;
+        if (_timer > AttackPeriod)
+        {
+            TargetBuilding.TakeDamage(Damage);
+            _timer = 0;
+        }
+    }
+
+    private void EnemyWalkToUnit()
+    {
+        if (!TargetUnit)
+        {
+            SetState(EnemyState.WalkToBuilding);
+            return;
+        }
+
+        NavMeshAgent.SetDestination(TargetUnit.transform.position);
+        float distance = Vector3.Distance(transform.position, TargetUnit.transform.position);
+
+        if (distance > FollowRange)
+            SetState(EnemyState.WalkToBuilding);
+        if (distance < AttackRange)
+        {
+            SetState(EnemyState.AttackUnit);
+        }
+    }
+
+    private void EnemyAttackUnit()
+    {
+        if (!TargetUnit)
+        {
+            SetState(EnemyState.WalkToBuilding);
+            return;
+        }
+
+        NavMeshAgent.SetDestination(TargetUnit.transform.position);
+
+        float distance = Vector3.Distance(transform.position, TargetUnit.transform.position);
+
+        if (distance > AttackRange)
+        {
+            SetState(EnemyState.WalkToUnit);
+        }
+
+        _timer += Time.deltaTime;
+        if (_timer > AttackPeriod)
+        {
+            TargetUnit.TakeDamage(Damage);
+            _timer = 0;
+        }
+    }
+
+    public void SetState(EnemyState newState)
+    {
+        CurrentEnemyState = newState;
+
+        switch (CurrentEnemyState)
+        {
+            case EnemyState.Idle:
+                FindClosestBuilding();
+
+                break;
+            case EnemyState.WalkToBuilding:
+                {
+                    FindClosestBuilding();
+
+                    if (TargetBuilding)
+                        NavMeshAgent.SetDestination(TargetBuilding.transform.position);
+                    else
+                        SetState(EnemyState.Idle);
+
+                    break;
+                }
+            case EnemyState.AttackBuilding:
+                _timer = 0;
+                break;
+            case EnemyState.WalkToUnit:
+                break;
+            case EnemyState.AttackUnit:
+                _timer = 0;
+                break;
+        }
+    }
+
+    public void FindClosestBuilding()
+    {
+        //Building[] allBuildings = FindObjectsOfType<Building>();
+
+        //float minDistance = Mathf.Infinity;
+        Building closestBuilding = Management.GetClosestBuilding<Building>(transform.position, out float minDistance);
+
+        //foreach (Building building in Management.AllBuildings)
+        //{
+        //    if (building.CurrentState == BuildingState.Placed)
+        //        continue;
+
+        //    float distance = Vector3.Distance(transform.position, building.transform.position);
+
+        //    if (distance < minDistance)
+        //    {
+        //        minDistance = distance;
+        //        closestBuilding = building;
+        //    }
+        //}
+
+        TargetBuilding = closestBuilding;
+    }
+
+    public void FindClosestUnit()
+    {
+        //Unit[] allUnits = FindObjectsOfType<Unit>();
+
+        //float minDistance = Mathf.Infinity;
+        Unit closestUnit = Management.GetClosestUnit<Unit>(transform.position, out float minDistance);
+
+        //foreach (Unit unit in Management.AllUnits)
+        //{
+        //    float distance = Vector3.Distance(transform.position, unit.transform.position);
+
+        //    if (distance < minDistance)
+        //    {
+        //        minDistance = distance;
+        //        closestUnit = unit;
+        //    }
+        //}
+
+        if (minDistance < FollowRange)
+        {
+            TargetUnit = closestUnit;
+            SetState(EnemyState.WalkToUnit);
+        }
+    }
+
+    public void TakeDamage(int damageValue)
+    {
+        //FindClosestUnit();
+
+        Health -= damageValue;
+        _healthBar.SetHealth(Health, _maxHealth);
+
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public virtual void OnDestroy()
+    {
+        if (_healthBar)
+            Destroy(_healthBar.gameObject);
+
+        Management.RemoveEnemy(this);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(transform.position, Vector3.up, AttackRange);
+
+        Handles.color = Color.yellow;
+        Handles.DrawWireDisc(transform.position, Vector3.up, FollowRange);
+    }
+#endif
+}
---
+++ Assets/Scripts/Units/Miner.cs
@@ -0,0 +1,248 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Miner : Unit
+{
+    public float FearRange = 6;
+    public float MiningRange = 1;
+    public float SeeMineRange = 8;
+
+    private Mine _targetMine;
+
+    private Enemy _scaredEnemy;
+    private Knight _knight;
+
+    public override void Start()
+    {
+        base.Start();
+
+        SetState(UnitState.Idle);
+    }
+    void Update()
+    {
+        if (CurrentUnitState != UnitState.Escape)
+            FindClosestEnemy();
+
+        switch (CurrentUnitState)
+        {
+            case UnitState.Idle:
+                MinerIdle();
+                    break;
+            case UnitState.WalkToPoint:
+                MinerWalkToPoint();
+                    break;
+            case UnitState.Mining:
+                MinerMining();
+                    break;
+            case UnitState.Escape:
+                MinerEscape();
+                break;
+        }
+    }
+
+    private void MinerIdle()
+    {
+        FindClosestMine();
+    }
+
+    private void MinerWalkToPoint()
+    {
+        if (!_targetMine)
+        {
+            if (Vector3.Distance(transform.position, TargetPoint) >= 0.5f)
+            {
+                NavMeshAgent.SetDestination(TargetPointer.transform.position);
+                SetState(UnitState.WalkToPoint);
+            }
+            else
+                SetState(UnitState.Idle);
+        }
+        else
+        {
+            NavMeshAgent.SetDestination(_targetMine.transform.position);
+            float distance = Vector3.Distance(transform.position, _targetMine.transform.position);
+
+            if (distance < MiningRange)
+            {
+                //_targetMine.ToggleIndicator(true);
+                SetState(UnitState.Mining);
+            }
+        }
+    }
+
+    private void MinerMining()
+    {
+    }
+
+    private void MinerEscape()
+    {
+        if (!FindClosestEnemy())
+            SetState(UnitState.WalkToPoint);
+
+        if (FindClosestKnight())
+        {
+            if (_knight.CurrentUnitState != UnitState.WalkToEnemy && _knight.CurrentUnitState != UnitState.Attack)
+                NavMeshAgent.SetDestination(_knight.transform.position);
+        }
+        else
+        {
+            Vector3 toEnemy = _scaredEnemy.transform.position - transform.position;
+            Vector3 fromEnemy = transform.position - toEnemy;
+
+            NavMeshAgent.SetDestination(fromEnemy);
+        }
+
+    }
+
+    public void SetState(UnitState newState)
+    {
+        CurrentUnitState = newState;
+
+        switch (CurrentUnitState)
+        {
+            case UnitState.Idle:
+                TargetPointer.SetActive(false);
+                break;
+            case UnitState.WalkToPoint:
+                break;
+            case UnitState.Mining:
+                {
+                    TargetPointer.SetActive(false);
+                    _targetMine.IncCountMiners();
+
+                    break;
+                }
+            case UnitState.Escape:
+                {
+                    TargetPointer.SetActive(false);
+                    LoseMine();
+
+                    break;
+                }
+        }
+    }
+
+    public bool FindClosestEnemy()
+    {
+        //Enemy[] allEnemies = FindObjectsOfType<Enemy>();
+
+        //float minDistance = Mathf.Infinity;
+        Enemy closestEnemy = Management.GetClosestEnemy(transform.position, out float minDistance);
+
+        //foreach (Enemy enemy in Management.AllEnemies)
+        //{
+        //    float distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+        //    if (distance < minDistance)
+        //    {
+        //        minDistance = distance;
+        //        closestEnemy = enemy;
+        //    }
+        //}
+
+        if (minDistance < FearRange)
+        {
+            _scaredEnemy = closestEnemy;
+            SetState(UnitState.Escape);
+
+            return true;
+        }
+
+        return false;
+    }
+    public bool FindClosestKnight()
+    {
+        //float minDistance = Mathf.Infinity;
+        Knight closestKnight = Management.GetClosestUnit<Knight>(transform.position, out float minDistance);
+
+        //foreach (Unit knight in Management.AllUnits)
+        //{
+        //    if (!(knight is Knight))
+        //        continue;
+
+        //    float distance = Vector3.Distance(transform.position, knight.transform.position);
+
+        //    if (distance < minDistance)
+        //    {
+        //        minDistance = distance;
+        //        closestKnight = (Knight)knight;
+        //    }
+        //}
+
+        if (closestKnight)
+        {
+            _knight = closestKnight;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public void FindClosestMine()
+    {
+        //Mine[] allMines = FindObjectsOfType<Mine>();
+
+        //float minDistance = Mathf.Infinity;
+        Mine closestMine = Management.GetClosestBuilding<Mine>(transform.position, out float minDistance);
+
+        //foreach (Building mine in Management.AllBuildings)
+        //{
+        //    if (!(mine is Mine))
+        //        continue;
+
+        //    if (mine.CurrentState == BuildingState.Placed)
+        //        continue;
+
+        //    float distance = Vector3.Distance(transform.position, mine.transform.position);
+
+        //    if (distance < minDistance)
+        //    {
+        //        minDistance = distance;
+        //        closestMine = (Mine)mine;
+        //    }
+        //}
+
+        if (minDistance < SeeMineRange)
+        {
+            _targetMine = closestMine;
+
+            SetState(UnitState.WalkToPoint);
+        }
+    }
+
+    public override void WhenClickOnGround(Vector3 point)
+    {
+        base.WhenClickOnGround(point);
+
+        LoseMine();
+    }
+
+    private void LoseMine()
+    {
+        _targetMine?.DecCountMiners();
+
+        _targetMine = null;
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        LoseMine();
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(transform.position, Vector3.up, FearRange);
+
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(transform.position, Vector3.up, MiningRange);
+    }
+#endif
+}
