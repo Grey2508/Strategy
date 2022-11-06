de07f6d853558bb08d8f93d8d819d30d5adbff07
Grey2508 <grey-_@mail.ru>
2022-11-07 03:42:33 +0500

    Update EnemyCreator.cs
    Delete unsing usings

--- Assets/Scripts/Building/EnemyCreator.cs
+++ Assets/Scripts/Building/EnemyCreator.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyCreator : MonoBehaviour
@@ -13,7 +11,7 @@ public class EnemyCreator : MonoBehaviour
     {
         _timer += Time.deltaTime;
 
-        if(_timer>Period)
+        if (_timer > Period)
         {
             Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
             _timer = 0;
