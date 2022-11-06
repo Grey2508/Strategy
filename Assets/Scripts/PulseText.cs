3a94347167b1c6c44f1c10a7253092f45e7a97f3
Grey2508 <grey-_@mail.ru>
2022-11-06 05:28:21 +0500

    Add PulseText effect

---
+++ Assets/Scripts/PulseText.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PulseText : MonoBehaviour
+{
+    public Text Text;
+    public int MaxSize = 300;
+    public int MinSize = 40;
+    public int Speed = 100;
+    public float TimeAnimation = 1;
+    public bool Infinitely = false;
+
+    private float _timer;
+    private bool _bigger = true;
+
+    public void StartEffect()
+    {
+        gameObject.SetActive(true);
+
+        StartCoroutine(ShowEffect());
+    }
+
+    private IEnumerator ShowEffect()
+    {
+
+        for (float t = 0; t < TimeAnimation; t += Time.deltaTime)
+        {
+            _timer += Time.deltaTime;
+
+            int size = Mathf.FloorToInt(_bigger ? MinSize + _timer * Speed : MaxSize - _timer * Speed);
+            Text.fontSize = size;
+
+            if (size > MaxSize || size < MinSize)
+            {
+                _bigger = !_bigger;
+
+                _timer = 0;
+            }
+
+            yield return null;
+        }
+
+        if (!Infinitely)
+            gameObject.SetActive(false);
+    }
+}
